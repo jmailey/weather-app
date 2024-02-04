5bc769525f3784791b660724e4322d02a8e4bbd9
jmailey <johnmmailey@gmail.com>
2024-02-04 18:42:36 +0000

    fix useWeatherAPI

--- src/hooks/useWeatherAPI/useWeatherAPI.ts
+++ src/hooks/useWeatherAPI/useWeatherAPI.ts
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import getFiveDayForecast from "../../api/getFiveDayForecast";
-import { Coord } from "../../api/responseTypes";
+import { Coord, WeatherApiResponse } from "../../api/responseTypes";
 
 export default function useWeatherAPI() {
   const [fetching, setFetching] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [forecast, setForecast] = useState<WeatherApiResponse | null>(null);
 
   const fetchForecast = async ({ lat, lon }: Coord) => {
     try {
       setFetching(true);
       const response = await getFiveDayForecast(lat, lon);
       setFetching(false);
+      setForecast(response);
       return response;
     } catch (error) {
       let message = "Unknown Error";
@@ -23,5 +25,6 @@ export default function useWeatherAPI() {
     fetchForecast,
     fetching,
     errorMessage,
+    forecast,
   };
 }
